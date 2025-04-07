f2db35015be5af82242d489bb68c39b8d90b70ac
Iványi Kristóf <pcgkhd@inf.elte.hu>
2025-04-07 18:37:55 +0200

    :spotbugs:spotlessApply

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/ReadReturnShouldBeChecked.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/ReadReturnShouldBeChecked.java
@@ -190,10 +190,9 @@ public class ReadReturnShouldBeChecked extends BytecodeScanningDetector implemen
                         new BugInstance(this, "SR_NOT_CHECKED", (wasBufferedInputStream ? HIGH_PRIORITY
                                 : recentCallToAvailable ? LOW_PRIORITY : NORMAL_PRIORITY)).addClassAndMethod(this)
                                 .addCalledMethod(lastCallClass, lastCallMethod, lastCallSig, false), SourceLineAnnotation
-                                .fromVisitedInstruction(getClassContext(), this, locationOfCall));
+                                        .fromVisitedInstruction(getClassContext(), this, locationOfCall));
             }
         }
-
         sawRead = false;
         sawSkip = false;
     }
